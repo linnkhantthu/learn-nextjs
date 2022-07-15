ac2e28ba5d5bb5809a3fb610a30be347a24e43d0
linnkhantthu <linnkhantthu1999mdy@gmail.com>
2022-07-15 20:17:51 +0630

    added_article_head

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,9 +1,13 @@
 import 'isomorphic-fetch'
+import Head from 'next/head'
 import { ArticleItem } from '../../../components/ArticleItem'
 
 export default function article({article}) {
     return (
         <div>
+        <Head>
+          <title>{article.title}</title>
+        </Head>
             <h1>{article.title}</h1>
             <p>{article.body}</p>
         </div>
